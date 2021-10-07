1c8621390e9e31c369cb8a8343b6e87508507bbb
NerdBird <nerdbird@pop-os.localdomain>
2021-10-06 21:58:49 -0500

    Replace some parts in main with regex and fix error causing fails

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ from twython import Twython
 import os
 import particle
 import sys
+import re
 
 BOTNAME = "tmbgcompleter"   # bot's twitter handle
 OWNERNAME = "guitar_budgie" # bot's owners' twitter handle
@@ -39,11 +40,14 @@ class MentionStream(TwythonStreamer):
         if (BOTNAME in mentionedppl and username!=BOTNAME):  
           print("It's a mention!")
 
+          # TODO: is this even needed once better matching is implemented????
           #Remove tags from text
           prevlyric = data["text"]
-          prevlyric = prevlyric.replace("@","")
-          for name in mentionedppl:
-            prevlyric = prevlyric.replace(name, "")
+          prevlyric = re.sub("\s?@[\w]+\s?", " ", prevlyric)
+          prevlyric = re.sub("\n", " ", prevlyric) # fix bug causing fails
+          # prevlyric = prevlyric.replace("@","")
+          # for name in mentionedppl:
+          #   prevlyric = prevlyric.replace(name, "")
           prevlyric = prevlyric.strip()
 
           if (len(data['text'])>tagged_length and prevlyric.lower()=='id'):  # if user just wants to id the song
@@ -62,9 +66,11 @@ class MentionStream(TwythonStreamer):
             else: # has lyrics above_data["text"]
               #Remove tags from text
               prevlyric = above_data["text"]
-              prevlyric = prevlyric.replace("@","")
-              for name in above_mentioned:
-                prevlyric = prevlyric.replace(name, "")
+              prevlyric = re.sub("\s?@[\w]+\s?", " ", prevlyric)
+              prevlyric = re.sub("\n", " ", prevlyric) # fix bug causing fails
+              # prevlyric = prevlyric.replace("@","")
+              # for name in above_mentioned:
+              #   prevlyric = prevlyric.replace(name, "")
               prevlyric = prevlyric.strip()
                             
               #Find the lyrics to the song
@@ -131,9 +137,11 @@ class MentionStream(TwythonStreamer):
             else: # has lyrics above_data["text"]
               #Remove tags from text
               prevlyric = above_data["text"]
-              prevlyric = prevlyric.replace("@","")
-              for name in above_mentioned:
-                prevlyric = prevlyric.replace(name, "")
+              prevlyric = re.sub("\s?@[\w]+\s?", " ", prevlyric)
+              prevlyric = re.sub("\n", " ", prevlyric) # fix bug causing fails
+              # prevlyric = prevlyric.replace("@","")
+              # for name in above_mentioned:
+              #   prevlyric = prevlyric.replace(name, "")
               prevlyric = prevlyric.strip()
               
               #Find the lyrics to the song
